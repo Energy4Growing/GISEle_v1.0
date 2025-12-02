ae4f7f7c9d35cdbb74235411c6115531ebba6d2e
CorradoMariaCaminiti <corradocaminiti@gmail.com>
2025-12-02 10:12:53 +0100

    Clustering not working
    This is the clustering not working. I am pushing it just to start working on the ws

--- gisele/preprocessing.py
+++ gisele/preprocessing.py
@@ -10,7 +10,8 @@ def create_folder(case_study, country,crs):
     cluster_folder = os.path.join(database,country,'Clusters','Communities.shp')
     substations_folder = os.path.join(database,country,'Substations','con_points.shp')
     study_area_folder = os.path.join(database,country,'Study_area','Study_area.shp')
-
+    if not os.path.exists(study_area_folder): 
+        study_area_folder = os.path.join(database,country,'Study_area','Study_area.gpkg')
     if not os.path.exists(os.path.join(gisele_folder,'Case studies',case_study)): # if this is a new project, create the starting point for the analysis
         # Create new folders for the study case
         os.makedirs(os.path.join(gisele_folder,'Case studies',case_study))
@@ -38,16 +39,19 @@ def create_folder(case_study, country,crs):
         Substations['Y'] = [Substations_crs['geometry'].values[i].xy[1][0] for i in range(Substations.shape[0])]
         Substations.to_file(os.path.join(gisele_folder,'Case studies', case_study, 'Input', 'substations'))
         # Read the polygon of the study area and write it in the local database.
-        study_area = gpd.read_file(study_area_folder)
+        study_area = gpd.read_file(study_area_folder).to_crs(crs)
         study_area.to_file(os.path.join(gisele_folder,'Case studies',case_study,'Input','Study_area'))
         # Read the communities and write them in the local database
-        Clusters = gpd.read_file(cluster_folder)
-        Clusters = Clusters.to_crs(crs)
-        Clusters['cluster_ID'] = [*range(1, Clusters.shape[0] + 1)]
-        for i, row in Clusters.iterrows(): # this is just in case one of the polygons is saved as a MP with just 1 polygon
-            if row['geometry'].geom_type == 'MultiPolygon':
-                Clusters.loc[i, 'geometry'] = row['geometry'][0]
-        Clusters.to_file(os.path.join(gisele_folder,'Case studies',case_study,'Input','Communities_boundaries'))
+        try: 
+            Clusters = gpd.read_file(cluster_folder)
+            Clusters = Clusters.to_crs(crs)
+            Clusters['cluster_ID'] = [*range(1, Clusters.shape[0] + 1)]
+            for i, row in Clusters.iterrows(): # this is just in case one of the polygons is saved as a MP with just 1 polygon
+                if row['geometry'].geom_type == 'MultiPolygon':
+                    Clusters.loc[i, 'geometry'] = row['geometry'][0]
+            Clusters.to_file(os.path.join(gisele_folder,'Case studies',case_study,'Input','Communities_boundaries')) 
+        except: 
+            Clusters = True
     else: # not a new project, just read the files from the local folder
         Clusters = gpd.read_file(os.path.join(gisele_folder,'Case studies',case_study,'Input','Communities_boundaries','Communities_boundaries.shp'))
         study_area = gpd.read_file(os.path.join(gisele_folder,'Case studies',case_study,'Input','Study_area','Study_area.shp'))
